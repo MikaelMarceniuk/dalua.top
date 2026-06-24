f26dff6b5e308c9a3d55b9e2e014cf3f9e97607f
MikaelMarceniuk <mika.marceniuk@gmail.com>
2026-06-24 06:53:51 -0300

    feat: Added verify-email logic

--- src/lib/auth.lib.ts
+++ src/lib/auth.lib.ts
@@ -1,8 +1,9 @@
+import { sentinelClient } from '@better-auth/infra/client'
 import { nextCookies } from 'better-auth/next-js'
 import { createAuthClient } from 'better-auth/react'
 
 export const authClient = createAuthClient({
-  plugins: [nextCookies()],
+  plugins: [sentinelClient(), nextCookies()],
   baseURL: process.env.NEXT_PUBLIC_APP_URL,
 })
 
--- src/server/auth.tsx
+++ src/server/auth.tsx
@@ -1,8 +1,12 @@
-import { db } from '@/server/db'
 import { betterAuth } from 'better-auth'
 import { drizzleAdapter } from 'better-auth/adapters/drizzle'
-import * as schema from '@/server/db/schemas'
 import { nextCookies } from 'better-auth/next-js'
+import { db } from '@/server/db'
+import * as schema from '@/server/db/schemas'
+import { resend } from './providers/email.provider'
+import VerifyEmailTemplate, {
+  VerifyEmailTemplateId,
+} from './emails/verify-email.template'
 
 export const auth = betterAuth({
   plugins: [nextCookies()],
@@ -14,8 +18,19 @@ export const auth = betterAuth({
   }),
   emailAndPassword: {
     enabled: true,
-    requireEmailVerification: false,
+    autoSignIn: true,
+    requireEmailVerification: true,
     minPasswordLength: 8,
+  },
+  emailVerification: {
+    sendVerificationEmail: async ({ user, url }) => {
+      void resend.emails.send({
+        from: 'Noreply <noreply@mmarceniuk.dev>',
+        subject: 'Verifique seu email — Dalua.top',
+        to: user.email,
+        react: <VerifyEmailTemplate verificationUrl={url} />,
+      })
+    },
   },
   session: {
     expiresIn: 60 * 60 * 24 * 7, // 7 dias
